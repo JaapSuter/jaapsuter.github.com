8d4b3c70e41ee466f88a46c31f70dde266717f94
Jaap Suter <github@jaapsuter.com>
2011-06-10 15:31:19 -0700

    Added a readme, fixed the image build script.

--- source/rebuild-images.cmd
+++ source/rebuild-images.cmd
@@ -3,5 +3,5 @@ set IMAGE_MAGICK_CONVERT_EXE=.\Tools\ImageMagick-6.7.0-Q16-Windows-Convert.exe
 cls
 rmdir /s /q ..\images
 xcopy /s /i .\images ..\images
-for /r ..\images %%X in (*.jpg) do (%IMAGE_MAGICK_CONVERT_EXE% %%X -resize 640x4096 %%X -verbose -monitor)
-for /r ..\images %%X in (*.png) do (%IMAGE_MAGICK_CONVERT_EXE% %%X -resize 640x4096 %%X -verbose -monitor)
+for /r ..\images %%X in (*.jpg) do (%IMAGE_MAGICK_CONVERT_EXE% %%X -verbose -resize 640x4096 %%X)
+for /r ..\images %%X in (*.png) do (%IMAGE_MAGICK_CONVERT_EXE% %%X -verbose -resize 640x4096 %%X)
